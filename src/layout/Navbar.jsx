1ff0b39e2fbc2494d82f0c69918bb34823e41e55
tonmoydeb404 <tonmoydeb404@gmail.com>
2022-10-14 01:46:52 +0600

    css fixed

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -6,9 +6,9 @@ const Navbar = () => {
   const { theme, toggleSidebar, toggleTheme } = useGlobalContext();
 
   return (
-    <div className="bg-white flex items-center gap-4 px-5 py-2 md:px-10 sticky top-0 left-0 w-full lg:hidden border-b border-b-gray-200">
+    <div className="bg-white flex items-center gap-4 px-5 py-2 md:px-10 sticky top-0 left-0 w-full lg:hidden border-b border-b-gray-200 dark:border-b-gray-700 dark:bg-slate-800 z-[1000]">
       <button
-        className="px-3 py-1.5 bg-slate-800 hover:bg-slate-900 duration-300 text-lg text-white rounded"
+        className="px-3 py-1.5 bg-slate-800 dark:bg-slate-700 hover:bg-slate-900 dark:hover:bg-slate-600 duration-300 text-lg text-white rounded"
         onClick={toggleSidebar}
       >
         <i className="bi bi-list"></i>
