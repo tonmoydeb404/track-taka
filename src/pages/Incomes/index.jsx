9373db58589a3d28e4e5223f9edbf5bd8ecba86a
tonmoydeb404 <tonmoydeb404@gmail.com>
2022-10-20 02:39:09 +0600

    transection pages updated

--- src/pages/Incomes/index.jsx
+++ src/pages/Incomes/index.jsx
@@ -1,11 +1,21 @@
 import React, { useMemo } from "react";
 import MonthFilter from "../../common/components/MonthFilter";
+import StatCard from "../../common/components/StatCard";
 import TransectionTable from "../../common/components/TransectionTable";
 import { useTransectionContext } from "../../common/contexts/transectionContext";
 
 const Incomes = () => {
   const { filteredState: state } = useTransectionContext();
 
+  // total incomes
+  const income = state.reduce((prev, current) => {
+    if (current.type == "income") {
+      return prev + current.amount;
+    }
+    return prev;
+  }, 0);
+
+  // income data
   const incomesData = useMemo(
     () => state.filter((item) => item.type == "income"),
     [state]
@@ -21,6 +31,15 @@ const Incomes = () => {
         <div className="flex items-center gap-2">
           <MonthFilter />
         </div>
+      </div>
+
+      <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-5 mt-10">
+        <StatCard
+          type="income"
+          title="income"
+          amount={income}
+          icon="graph-up-arrow"
+        />
       </div>
 
       <TransectionTable className="mt-10" data={incomesData} />
--- src/pages/Transections/index.jsx
+++ src/pages/Transections/index.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import MonthFilter from "../../common/components/MonthFilter";
+import StatCard from "../../common/components/StatCard";
 import TransectionTable from "../../common/components/TransectionTable";
 import { useTransectionContext } from "../../common/contexts/transectionContext";
 
 const Transections = () => {
   const { filteredState: state } = useTransectionContext();
+
+  const income = state.reduce((prev, current) => {
+    if (current.type == "income") {
+      return prev + current.amount;
+    }
+    return prev;
+  }, 0);
+
+  const expense = state.reduce((prev, current) => {
+    if (current.type == "expense") {
+      return prev + current.amount;
+    }
+    return prev;
+  }, 0);
 
   return (
     <>
@@ -16,6 +31,22 @@ const Transections = () => {
         <div className="flex items-center gap-2">
           <MonthFilter />
         </div>
+      </div>
+
+      <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-5 mt-10">
+        <StatCard
+          type="income"
+          title="income"
+          amount={income}
+          icon="graph-up-arrow"
+        />
+
+        <StatCard
+          type="expense"
+          title="expense"
+          amount={expense}
+          icon="graph-down-arrow"
+        />
       </div>
 
       <TransectionTable className="mt-10" data={state} />
