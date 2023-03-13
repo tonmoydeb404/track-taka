25ae6e3b23a7ba8744d3d2f7cd9a39b7065a28c6
Tonmoy Deb <tonmoydeb404@gmail.com>
2023-03-13 19:55:34 +0600

    settings page updated

--- src/pages/Settings/index.jsx
+++ src/pages/Settings/index.jsx
@@ -1,22 +1,11 @@
 import React from "react";
-import {
-  BsBoxArrowRight,
-  BsCloudDownload,
-  BsCloudUpload,
-} from "react-icons/bs";
+import { BsBoxArrowRight } from "react-icons/bs";
 import { useAuth } from "../../common/contexts/AuthContext";
 import { useGlobal } from "../../common/contexts/GlobalContext";
-import { useTransection } from "../../common/contexts/TransectionContext";
-import {
-  downloadTransections,
-  uploadTransections,
-} from "../../common/services/transectionServices";
 
 const Settings = () => {
   const { user, handleLogOut } = useAuth();
-  const { transections, insertTransection, clearTransection } =
-    useTransection();
-  const { autoBackup, setAutoBackupDuration, isOnline } = useGlobal();
+  const { isOnline } = useGlobal();
 
   return (
     <>
@@ -44,70 +33,6 @@ const Settings = () => {
                   <BsBoxArrowRight />
                 </button>
               </div>
-
-              {isOnline ? (
-                <>
-                  <div className="flex flex-col gap-1.5">
-                    <div className="settings_item ">
-                      <h2>Automatic backup data in</h2>
-
-                      <select
-                        className="ml-auto text-sm py-1 pl-2 select"
-                        value={autoBackup.duration || 1}
-                        onChange={(e) => {
-                          setAutoBackupDuration(parseFloat(e.target.value));
-                        }}
-                      >
-                        <option value={1}>01 Days</option>
-                        <option value={2}>02 Days</option>
-                        <option value={3}>03 Days</option>
-                      </select>
-                    </div>
-                    {autoBackup.lastTime && (
-                      <p className="text-xs pl-2 text-gray-600 dark:text-gray-300 ">
-                        last backup at{" "}
-                        {new Date(autoBackup.lastTime).toLocaleString()}
-                      </p>
-                    )}
-                  </div>
-
-                  <div className="settings_item">
-                    <h2>Backup Data Now</h2>
-
-                    <button
-                      className="btn btn-icon btn-success ml-auto"
-                      onClick={() =>
-                        uploadTransections({
-                          uid: user?.uid,
-                          data: transections,
-                        })
-                      }
-                    >
-                      <BsCloudUpload />
-                    </button>
-                  </div>
-
-                  <div className="settings_item">
-                    <h2>Download Data</h2>
-
-                    <button
-                      className="btn btn-icon btn-warning ml-auto"
-                      onClick={() =>
-                        downloadTransections({
-                          uid: user?.uid,
-                          updateState: insertTransection,
-                        })
-                      }
-                    >
-                      <BsCloudDownload />
-                    </button>
-                  </div>
-                </>
-              ) : (
-                <>
-                  <p>make sure your internet connection is stable</p>
-                </>
-              )}
             </>
           ) : (
             <>
