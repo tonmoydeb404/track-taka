7289cf464c0fb34977b5e7d43df634cbde23a9f9
Tonmoy Deb <tonmoydeb404@gmail.com>
2023-03-21 20:41:03 +0600

    added old db to delete

--- src/config/indexedDB.config.js
+++ src/config/indexedDB.config.js
@@ -2,7 +2,7 @@ const indexedDBConfig = {
   NAME: import.meta.env.MODE === "production" ? "TRACK_TAKA" : "DEV-TRACK_TAKA",
   VERSION: 1,
   STORE: "transections",
-  OLD_STORE: "",
+  OLD_STORE: "TRACK_TAKA_DB",
   KEY_PATH: "id",
 };
 
